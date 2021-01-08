60cf37106486e979fb8d0e74a7705ab88a175a01
ROBSEPICGAMINGB\Rob <thundergod6115@hotmail.com>
2021-01-08 14:30:25 +0000

    Add project files.

---
+++ FunPetPics/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FunPetPics.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
