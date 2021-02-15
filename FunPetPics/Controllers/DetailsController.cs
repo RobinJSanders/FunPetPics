9a0b9225f4848cdc6b0e594a4c5515a2ef626881
ROBSEPICGAMINGB\Rob <thundergod6115@hotmail.com>
2021-02-15 14:04:20 +0000

    Add details page and allow users to view their rating (WIP)

---
+++ FunPetPics/Controllers/DetailsController.cs
@@ -0,0 +1,29 @@
+﻿using FunPetPics.Data;
+using FunPetPics.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace FunPetPics.Controllers
+{
+    public class DetailsController : ControllerBase
+    {
+        public DetailsController(FunPetPicsContext context) : base(context)
+        {
+        }
+
+        [HttpGet]
+        [Route("Details/Rate/{id}")]// when I add this the controller doesn't work and I get page not found
+        public IActionResult Rate(int id)
+        {
+            var model = new Tuple<PetPhotoModel, UserModel>
+           (
+                _context.PetPhotos.Include(e => e.Ratings).FirstOrDefault(p => p.Id == id),
+                GetLoggedInUser()
+           );
+
+            return View(model);
+        }
+    }
+}
